d11eb2ef30c47e1d2c99e21388c486ef8ceb56f7
= <=>
2023-05-10 18:59:46 -0400

    updated code to allow for accurate movement of the snake

--- client.js
+++ client.js
@@ -1,5 +1,21 @@
-const connectToServer = require("./play");
+const net = require("net");
 
-console.log("Connecting ...");
-const conn = connectToServer();
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("You are now connected!");
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
+
 
--- play.js
+++ play.js
@@ -1,41 +1,11 @@
-const net = require("net");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
 
-  conn.setEncoding("utf8");
 
-  conn.on("connect", () => {
-    console.log("You are now connected!");
+console.log("hello", connect);
+const connection = connect()
+setupInput(connection)
 
-    setInterval(() => {
-      conn.write("Move: up");
-    }, 50);
-  });
 
-  return conn;
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-  // Handle other key inputs as needed
-};
-
-const conn = connect();
-setupInput().on("data", handleUserInput);
-
-module.exports = connect;
 
